1a7ee83af8c10226f729f1dcb1d544e358acd9db
Ryan Kaye <ryan@Ryans-MBP.lan>
2024-02-02 20:43:44 +0000

    define app.js, passport and jwt auth

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ const LocalStrategy = require("passport-local").Strategy;
 const JWTstrategy = require("passport-jwt").Strategy;
 const ExtractJWT = require("passport-jwt").ExtractJwt;
 
-const apiRouter = require("./routes/api");
+const User = require("./models/user");
+
+// MONGOOSE
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  console.log("Successfully Connected to MongoDB");
+}
+
+main().catch((err) => console.log(err));
 
 var app = express();
 
@@ -23,11 +31,55 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// PASSPORT STUFF
+//Defined login strategy
+passport.use(
+  "login",
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: "User not found" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+//Defined JWT strategy
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+app.use(passport.initialize());
+const apiRouter = require("./routes/api");
+const indexRouter = require("./routes/index");
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
